12c32c63cdf8bc6e2585e05e877b6359a787a99a
romanovskaja <t.v.romanovskaya@tut.by>
2021-01-12 08:48:32 +0300

    add video

--- #src/js/script.js
+++ #src/js/script.js
@@ -79,79 +79,99 @@ $('.slider').slick({
 });
 
 
-let base = {
-	// Поиск элементов по классу
-	findClass: function(str, node) {
-		 if(document.getElementsByClassName) return (node || document).getElementsByClassName(str);
-		 else {
-			  let node = node || document, list = node.getElementsByTagName('*'), length = list.length, Class = str.split(/\s+/), classes = Class.length, array = [], i, j, key;
-			  for(i = 0; i < length; i++) {
-					key = true;
-					for(j = 0; j < classes; j++) if(list[i].className.search('\\b' + Class[j] + '\\b') == -1) key = false;
-					if(key) array.push(list[i]);
-			  }
-			  return array;
-		 }
-	},
-	// Добавление обработчиков событий
-	bind: function(node, type, listener) {
-		if(node.addEventListener) node.addEventListener(type, listener, false);
-		//@cc_on node.attachEvent('on' + type, function() { listener.call(node); });
-	},
-	// Реализация DOMContentLoaded
-	init: [],
-	ready: function() {
-		if(!arguments.callee.done) {
-			arguments.callee.done = true;
-			if(this.timer) clearInterval(this.timer);
-			let i, length = this.init.length;
-			for(i = 0; i < length; i++) this.init[i]();
-			this.init = [];
-		}
-	},
-	check: function() {
-		let _this = this, listener = function() {
-			_this.ready();
-		};
-		if(document.addEventListener) document.addEventListener('DOMContentLoaded', listener, false);
-		if(/KHTML|WebKit/i.test(navigator.userAgent)) this.timer = setInterval(function() {
-			if(/loaded|complete/.test(document.readyState)) base.ready();
-		}, 10);
-		/*@cc_on document.write(unescape('%3CSCRIPT onreadystatechange="if(this.readyState==\'complete\') base.ready()" defer=defer src=\/\/:%3E%3C/SCRIPT%3E')); @*/
-		this.bind(window, 'load', listener);
-	}
-};
+// let base = {
+// 	// Поиск элементов по классу
+// 	findClass: function(str, node) {
+// 		 if(document.getElementsByClassName) return (node || document).getElementsByClassName(str);
+// 		 else {
+// 			  let node = node || document, list = node.getElementsByTagName('*'), length = list.length, Class = str.split(/\s+/), classes = Class.length, array = [], i, j, key;
+// 			  for(i = 0; i < length; i++) {
+// 					key = true;
+// 					for(j = 0; j < classes; j++) if(list[i].className.search('\\b' + Class[j] + '\\b') == -1) key = false;
+// 					if(key) array.push(list[i]);
+// 			  }
+// 			  return array;
+// 		 }
+// 	},
+// 	// Добавление обработчиков событий
+// 	bind: function(node, type, listener) {
+// 		if(node.addEventListener) node.addEventListener(type, listener, false);
+// 		//@cc_on node.attachEvent('on' + type, function() { listener.call(node); });
+// 	},
+// 	// Реализация DOMContentLoaded
+// 	init: [],
+// 	ready: function() {
+// 		if(!arguments.callee.done) {
+// 			arguments.callee.done = true;
+// 			if(this.timer) clearInterval(this.timer);
+// 			let i, length = this.init.length;
+// 			for(i = 0; i < length; i++) this.init[i]();
+// 			this.init = [];
+// 		}
+// 	},
+// 	check: function() {
+// 		let _this = this, listener = function() {
+// 			_this.ready();
+// 		};
+// 		if(document.addEventListener) document.addEventListener('DOMContentLoaded', listener, false);
+// 		if(/KHTML|WebKit/i.test(navigator.userAgent)) this.timer = setInterval(function() {
+// 			if(/loaded|complete/.test(document.readyState)) base.ready();
+// 		}, 10);
+// 		/*@cc_on document.write(unescape('%3CSCRIPT onreadystatechange="if(this.readyState==\'complete\') base.ready()" defer=defer src=\/\/:%3E%3C/SCRIPT%3E')); @*/
+// 		this.bind(window, 'load', listener);
+// 	}
+// };
 
-// Функции для работы с панельками
-let videor = {
-	process: function() {
-		let i, list = base.findClass('videor'), length = list.length;
-		for(i = 0; i < length; i++) base.bind(list[i], 'click', this.video);
-		list = base.findClass('content');
-		length = list.length;
-		for(i = 0; i < length; i++) list[i].style.display = 'none';
-	},
-	video: function() {
-		let content = base.findClass('content', this.parentNode)[0], e = arguments[0] || window.event;
-		if(content.style.display == 'block') {
-			content.style.display = 'none';
-			this.innerHTML = 'show';
-		}
-		else {
-			content.style.display = 'block';
-			this.innerHTML = 'close';
-		}
-		e.preventDefault ? e.preventDefault() : e.returnValue = false;
-	}
-};
+// // Функции для работы с панельками
+// let videor = {
+// 	process: function() {
+// 		let i, list = base.findClass('videor'), length = list.length;
+// 		for(i = 0; i < length; i++) base.bind(list[i], 'click', this.video);
+// 		list = base.findClass('video-player');
+// 		length = list.length;
+// 		for(i = 0; i < length; i++) list[i].style.display = 'none';
+// 	},
+// 	video: function() {
+// 		let content = base.findClass('video-player', this.parentNode)[0], e = arguments[0] || window.event;
+// 		if(content.style.display == 'block') {
+// 			content.style.display = 'none';
+// 			this.innerHTML = 'show';
+// 		}
+// 		else {
+// 			content.style.display = 'block';
+// 			this.innerHTML = 'close';
+// 		}
+// 		e.preventDefault ? e.preventDefault() : e.returnValue = false;
+// 	}
+// };
 
-// Ищем блоки с классом «video» по событию DOMContentLoaded
-base.init.push(function() {
-	videor.process();
+// // Ищем блоки с классом «video» по событию DOMContentLoaded
+// base.init.push(function() {
+// 	videor.process();
+// });
+
+// // Запускаем проверку готовности DOM
+// base.check();
+let videoPlayer;
+let videoControl = document.querySelector('.videor');
+
+document.addEventListener("DOMContentLoaded", () => {
+  initialiseMediaPlayer();
 });
 
-// Запускаем проверку готовности DOM
-base.check();
+function initialiseMediaPlayer() {
+ videoPlayer = document.getElementById('video-player');
+ videoPlayer.controls = false;
+}
+
+videoControl.addEventListener('click', () => {
+ if (videoPlayer.paused || videoPlayer.ended) {
+  videoPlayer.play();
+ } else {
+  videoPlayer.pause();
+ }
+});
+
 
 
 // document.querySelector('#play').onclick = play
