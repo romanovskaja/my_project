2d51c7e9baf03a2d87d5250ef9c150e37eb702f1
romanovskaja <t.v.romanovskaya@tut.by>
2021-01-12 09:36:34 +0300

    video

--- #src/js/script.js
+++ #src/js/script.js
@@ -152,6 +152,8 @@ $('.slider').slick({
 
 // // Запускаем проверку готовности DOM
 // base.check();
+
+
 let videoPlayer;
 let videoControl = document.querySelector('.videor');
 
@@ -173,72 +175,6 @@ videoControl.addEventListener('click', () => {
 });
 
 
-
-// document.querySelector('#play').onclick = play
-// document.querySelector('#pause').onclick = pause
-// document.querySelector('#stop').onclick = stop
-// document.querySelector('#speed-up').onclick = speedUp
-// document.querySelector('#speed-down').onclick = speedDown
-// document.querySelector('#speed-normal').onclick = speedNormal
-// document.querySelector('#volume').oninput = videoVolume
-
-// let video
-// let display
-// let progress
-
-// video = document.querySelector('#video-player')
-// progress = document.querySelector('#progress')
-// progress.onclick = videoRewind
-
-// video.ontimeupdate = progressUpdate
-
-// function play() {
-//     video.play()
-// }
-
-// function pause() {
-//     video.pause()
-// }
-// function stop() {
-//     video.pause()
-//     video.currentTime = 0
-// }
-// function speedUp() {
-//     video.play()
-//     video.playbackRate = 5
-// }
-// function speedDown() {
-//     video.play()
-//     video.playbackRate = 0.5
-// }
-// function speedNormal() {
-//     video.play()
-//     video.playbackRate = 1
-// }
-// function videoVolume() {
-//     let v = this.value
-//     console.log(v)
-//     video.volume = v/100
-// }
-// function progressUpdate () {
-//     console.log (video.duration)
-//     console.log (video.currentTime)
-//     let d = video.duration
-//     let c = video.currentTime
-//     progress.value = (100*c)/d
-//     document.querySelector('#out').innerHTML = video.currentTime
-// }
-// function videoRewind () {
-//     let w = this.offsetWidth
-//     let o = event.offsetX
-//     console.log (w)
-//     console.log (o)
-//     this.value = 100*o/w
-//     video.pause()
-//     video.currentTime = video.duration * (o/w)
-//     video.play()
-
-// }
 
 let btnsFilterContainer = document.querySelector('.wr-filter');
 
