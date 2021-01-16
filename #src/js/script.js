d5ab9dc6dba7c536247e5a48a5eead57740df7d6
romanovskaja <t.v.romanovskaya@tut.by>
2021-01-16 23:27:44 +0300

    arrows

--- #src/js/script.js
+++ #src/js/script.js
@@ -224,7 +224,9 @@ let removeClass = function(arr, className) {
 $('.slider-posts').slick({
 	infinite: true,
 	slidesToShow: 3,
-	slidesToScroll: 1,
+  slidesToScroll: 1,
+  prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+  nextArrow: '<button class="slide-arrow next-arrow"></button>',
 	responsive: [
 	    {
 	      breakpoint: 768,
