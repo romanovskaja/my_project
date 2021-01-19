efa3cbd6893e7eee31b630c1577bf422fbe34b72
romanovskaja <t.v.romanovskaya@tut.by>
2021-01-19 22:16:14 +0300

    arrows

--- #src/js/script.js
+++ #src/js/script.js
@@ -225,8 +225,8 @@ $('.slider-posts').slick({
 	infinite: true,
 	slidesToShow: 3,
   slidesToScroll: 1,
-  prevArrow: '<button class="slide-arrow prev-arrow"></button>',
-  nextArrow: '<button class="slide-arrow next-arrow"></button>',
+  prevArrow: $('.prev'),
+  nextArrow: $('.next'),
 	responsive: [
 	    {
 	      breakpoint: 768,
